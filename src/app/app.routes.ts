7279f5d60baefb34b53359e272647592ed8f9668
leolopezv <leonardo.lopez@uees.edu.ec>
2024-06-18 16:35:14 -0500

    Subida inicial del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { BlogComponent } from '../app/views/blog/blog.component';
+import { ContactoComponent } from '../app/views/contacto/contacto.component';
+import { InicioComponent } from '../app/views/inicio/inicio.component';
+import { TiendaComponent } from '../app/views/tienda/tienda.component';
+
+export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'tienda', component: TiendaComponent}
+];
---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+import { BlogComponent } from '../../views/blog/blog.component';
+import { ContactoComponent } from '../../views/contacto/contacto.component';
+import { InicioComponent } from '../../views/inicio/inicio.component';
+import { TiendaComponent } from '../../views/tienda/tienda.component';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule, BlogComponent, ContactoComponent, InicioComponent, TiendaComponent, RouterModule, RouterOutlet],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
