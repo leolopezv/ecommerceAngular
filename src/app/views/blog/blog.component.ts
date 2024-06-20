295e1e99b2f75d571905f9f25c94a2d9dbf845f5
anapalacios364 <148572480+anapalacios364@users.noreply.github.com>
2024-06-20 01:45:52 -0500

    commit

--- src/app/views/blog/blog.component.ts
+++ src/app/views/blog/blog.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { RecursosService } from '../../servicios/recursos.service';
-import { BlogData } from '../../interfaz/foto';
+import { Usuarios } from '../../interfaz/usuarios';
 
 @Component({
   selector: 'app-blog',
+  standalone: true,
+  imports: [HttpClientModule, CommonModule],
+  providers: [RecursosService],
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
-export class BlogComponent implements OnInit {
-  blogData: BlogData | null = null;  // Cambiado a BlogData
 
-  constructor(private recursosService: RecursosService) {}
-
-  ngOnInit(): void {
-    this.recursosService.obtenerDatos().subscribe(respuesta => {
-      this.blogData = respuesta;
-    });
+export class BlogComponent {
+  title = 'Blog de usuarios';
+  usuarios: Usuarios[] = [];
+  constructor(private recursosService: RecursosService) {
+    recursosService.obtenerDatos().subscribe(respuesta => {
+      this.usuarios = respuesta as Array<Usuarios>;
+    })
   }
 }
