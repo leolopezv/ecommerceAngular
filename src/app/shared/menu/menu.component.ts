e947923078b59085428ab0b853add6ad323ed96e
leolopezv <leonardo.lopez@uees.edu.ec>
2024-06-19 22:58:26 -0500

    Componente carrito

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -6,11 +6,12 @@ import { BlogComponent } from '../../views/blog/blog.component';
 import { ContactoComponent } from '../../views/contacto/contacto.component';
 import { InicioComponent } from '../../views/inicio/inicio.component';
 import { TiendaComponent } from '../../views/tienda/tienda.component';
+import { CarritoComponent } from '../../views/carrito/carrito.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule, BlogComponent, ContactoComponent, InicioComponent, TiendaComponent, RouterModule, RouterOutlet],
+  imports: [CommonModule, BlogComponent, ContactoComponent, InicioComponent, TiendaComponent, CarritoComponent, RouterModule, RouterOutlet],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
